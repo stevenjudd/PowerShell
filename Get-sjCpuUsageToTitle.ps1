c898dd031b603f28eb4da30bf04ae8bd2da8a9f4
Steven Judd <stevenjudd@gmail.com>
2018-06-11 23:28:17 -0500

    Added the Get-sjCpuUsageToTitle function

---
+++ Get-sjCpuUsageToTitle.ps1
@@ -0,0 +1,22 @@
+﻿function Get-sjCpuUsageToTitle {
+
+    Start-Job -Name CpuMonitor -ScriptBlock {
+        while($true)
+        {
+            $cpuValue = (Get-WmiObject win32_processor).LoadPercentage
+            $Host.UI.RawUI.WindowTitle = "$env:username | PSVer: $($PSVersionTable.PsVersion.ToString()) | PID: $PID | CPU%: $cpuValue"
+            Start-Sleep -Seconds 15
+        }
+    } | Out-Null
+
+    if(-not($timer))
+    {
+        $timer = New-Object System.Timers.Timer
+        $action = {Receive-Job -Name CpuMonitor -Keep}
+        $timer.Interval = 15000
+        $timer.Enabled = $true
+        Register-ObjectEvent -InputObject $timer -EventName elapsed -SourceIdentifier CpuTimer -Action $action | Out-Null
+        $timer.Start()
+    }
+
+} #end Get-sjCpuUsageToTitle function 
