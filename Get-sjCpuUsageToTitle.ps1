e256fac34866477fbcb2964db01ba79e36201ca1
Steven Judd <stevenjudd@gmail.com>
2018-06-12 00:20:20 -0500

    added help block

--- Get-sjCpuUsageToTitle.ps1
+++ Get-sjCpuUsageToTitle.ps1
@@ -1,5 +1,27 @@
 ﻿function Get-sjCpuUsageToTitle {
 
+    <#
+    .Notes
+        Based on a script from Daniel S Potter:
+        https://twitter.com/DanielSPotter1/status/1005133432622592001
+        Modified by Steven Judd to be a function, add a parameter for computername, and output result as an object
+        Updated by [name] on [yyyy/mm/dd] to ...
+        Version 20180612.1 (please use yyyymmdd.x notation)
+    .SYNOPSIS
+        Function to create a backgroud job to update the window title with the CPU percent
+    .DESCRIPTION
+        This function will create a job and a system timer that will get the current
+        CPU percent and update the window title with the username, the PowerShell version,
+        the shell process ID, and the current CPU percent.
+    .LINK
+        https://github.com/stevenjudd/PowerShell/blob/master/Get-sjCpuUsageToTitle.ps1https://github.com/stevenjudd/PowerShell
+    .EXAMPLE
+        Get-sjCpuUsageToTitle
+        This command runs the function to start a background job that will update
+        the title of the window with the username, the PowerShell version, the shell
+        process ID, and the current CPU percent.
+    #>
+    
     Start-Job -Name CpuMonitor -ScriptBlock {
         while($true)
         {
