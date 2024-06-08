0093d55e47c2bdf3c8c7da62d3e7bbd5c37ff643
Steven Judd <stevenjudd@users.noreply.github.com>
2024-06-07 20:38:24 -0500

    converted to function

--- volumeScript.ps1
+++ volumeScript.ps1
@@ -1,21 +1,28 @@
-param(
-  [string]$action
-)
+function Send-sjVolumeKey {
+  param(
+    [Parameter(Mandatory = $true)]
+    [ValidateSet('VolumeUp', 'VolumeDown', 'Mute')]
+    [string]$action
+  )
 
-# Create a new COM object for WScript.Shell
-$shell = New-Object -ComObject wscript.shell
+  # Create a new COM object for WScript.Shell
+  $shell = New-Object -ComObject wscript.shell
 
-switch ($action) {
-  'volumeup' {
-    $shell.SendKeys([char]175)
-  }
-  'volumedown' {
-    $shell.SendKeys([char]174)
-  }
-  'mute' {
-    $shell.SendKeys([char]173)
-  }
-  default {
-    Write-Warning 'Invalid action. Use volumeup, volumedown, or mute.'
+  switch ($action) {
+    'VolumeUp' {
+      $shell.SendKeys([char]175)
+    }
+    'VolumeDown' {
+      $shell.SendKeys([char]174)
+    }
+    'Mute' {
+      $shell.SendKeys([char]173)
+    }
+    default {
+      Write-Output 'Invalid action. Use VolumeUp, VolumeDown, or Mute.'
+    }
   }
 }
+
+# Example usage
+Control-Volume -action 'VolumeUp'
